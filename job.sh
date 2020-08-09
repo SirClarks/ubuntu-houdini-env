b2a39e35a3e639d54c6ecbc8a3c48e25958f9d40
Kenny Clark <bigkenny15@yahoo.com>
2020-08-09 16:00:36 -0500

    Update job.sh
    Fixed formatting, added comments, added ENV variable for Houdini Backups Dir. This was causing issues putting the /hip/backup in the root of the shot folder.

--- job.sh
+++ job.sh
@@ -6,12 +6,14 @@ if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
   exit 0
 fi
 
+# history check
 if [ "$1" == "-hh" ] || [ "$1" == "-history" ] || [ "$1" == "--history" ]; then
   echo "Last 20 Shots: "
   echo "`tail -n 15 $HOME/FX/UTILITY/ENV/history.txt`"
   exit 0
 fi
 
+# last job/shot load via history.txt else use the two typed arguments
 if [ "$1" == "-l" ] || [ "$1" == "-l -l" ]; then
   LAST=$( tail -n 1 $HOME/FX/UTILITY/ENV/history.txt )
   echo "loading last shot: $LAST"
@@ -24,19 +26,20 @@ else
   SHOT=${2?Error: no shot given}
 fi
 
-# error check just in case for history call
+# error check just in case for -l call error
 if [ -z "$JOB" ] || [ -z "$SHOT" ]; then
-   echo "JOB and/or SHOT is empty, something went wrong, please try again not using -l or check the scripts"
-   exit 0
+  echo "JOB and/or SHOT is empty, something went wrong, please try again not using -l or check the scripts"
+  exit 0
 fi
 
+# set main path
 HPATH=$HOME/FX/PROJECTS/$JOB/$SHOT/
 
 if cd $HPATH; then
-	echo "Entering $JOB/$SHOT"
+  echo "Entering $JOB/$SHOT"
 else
-	echo "Could not find job/shot, run jobcreate or shotcreate if needed" 1>&2
-	exit 1
+  echo "Could not find job/shot, run jobcreate or shotcreate if needed" 1>&2
+  exit 1
 fi
 
 echo "Job set to: $JOB"
@@ -56,6 +59,7 @@ export GEO="$HPATH/houdini/geo/"
 export ASSETS="$HPATH/houdini/assets/"
 export RENDER="$HPATH/houdini/renders/"
 export TEXTURE="$HPATH/houdini/texture/"
+export HOUDINI_BACKUP_DIR="$HPATH/houdini/hip/backup"
 
 # set preferences
 export HOUDINI_NO_START_PAGE_SPLASH=1
@@ -67,16 +71,16 @@ export HOUDINI_BACKUP_DIR="$HPATH/hip/backup/"
 # per job overrides
 if [ $JOB == 'rnd' ]
 then
-	# export HOUDINI_SPLASH_FILE="/media/$USER/data/FX/jobs/config/splash/caveman.png"
-	export HOUDINI_SPLASH_MESSAGE="$JOB | $SHOT"
+  # export HOUDINI_SPLASH_FILE="/media/$USER/data/FX/jobs/config/splash/caveman.png"
+  export HOUDINI_SPLASH_MESSAGE="$JOB | $SHOT"
 else
-	export HOUDINI_SPLASH_MESSAGE="$JOB | $SHOT"
+  export HOUDINI_SPLASH_MESSAGE="$JOB | $SHOT"
 fi
 
 # write to file for history
 if [[ ! -e $HOME/FX/UTILITY/ENV/history.txt ]]; then
-    touch $HOME/FX/UTILITY/ENV/history.txt
-    echo "no history log found, created log at $HOME/FX/UTILITY/ENV/history.txt"
+  touch $HOME/FX/UTILITY/ENV/history.txt
+  echo "no history log found, created log at $HOME/FX/UTILITY/ENV/history.txt"
 fi
 
 echo "$JOB $SHOT" >> $HOME/FX/UTILITY/ENV/history.txt
